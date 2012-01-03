07d24699f03dcfdeccca9c9dcb993b8054702bc3
ruedin <ruedin@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-01-03 22:51:40 +0000

    removing broken files
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/agrmt@3 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/.R
+++
@@ -1,7 +0,0 @@
-polarization <-
-function (V, old=FALSE) {
-   # Calculates polarization (SOM Project)
-   # Arguments: as Agreement function (V = frequnecy vector)
-   p <- 2 - (agreement(V,old) + 1)  # pass arguments to agreement function, rescale
-   return(p)
-   }
