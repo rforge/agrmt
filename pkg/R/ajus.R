ad2b4abe480587b141b834051ee3f822d2783534
ruedin <ruedin@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-12-12 12:02:03 +0000

    0.25 AJUSF + L
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/agrmt@55 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/ajus.R
+++ pkg/R/ajus.R
@@ -1,19 +1,20 @@
 ajus <-
-function(V, tolerance=0.1) {
+function(V, tolerance=0.1, variant="modified") {
   # Determine the shape of a distribution
   # Arguments:         V = frequency vector
   #            tolerance = tolerance (absolute value)
+  #              variant = "strict" for AJUS, "modified" for AJUSFL
   # Example: V <- c(30,40,210,130,530,50,10)
   if (min(V) < 0) stop("Error: negative values found in frequency vector.") # input validation
   # (1) identify patterns: 0 flat, 1 increase, -1 decrease
   n <- length(V)    # number of items
   z <- n-1          # number of breaks
   if (n < 3) {warning("Warning: too few values to classify distribution.") # input validation
-      r <- list(type= NA, skew = NA) # returning NA for both type and skew (so we can use AJUS()$type)
+      r <- list(type=NA, skew=NA) # returning NA for both type and skew (so we can use AJUS()$type)
       return(r)}
   x <- NULL         # prepare
   for (i in 1:z) {
-    x[i] <- compareValues(V[i],V[i+1], tolerance=tolerance)
+    x[i] <- compareValues(V[i], V[i+1], tolerance=tolerance)
     }
   # Example vector V gives x = c(1  1 -1  1 -1 -1)
   # or with tolerance=.5:  x = c(0  1  0  1 -1 -1)
@@ -24,9 +25,9 @@ function(V, tolerance=0.1) {
     if (max.x <  1) A <- "J" else {      # no 1,  thus only 0 or + 1 (single peak at left end)  -- "L"
       if (min.x > -1) A <- "J"  else {   # no -1, thus only 0 or 1   (single peak at right end) -- "J"
         xs <- reduceVector(x)            # remove 0 and repeated values; not use unique(), because I want same values at different positions ("type S")
-        if (isTRUE(all.equal(xs,c(1,-1)))) A <- "A" else {
+        if (isTRUE(all.equal(xs, c(1,-1)))) A <- "A" else {
           # isTRUE(all.equal(V[i],m)
-          if (isTRUE(all.equal(xs,c(-1,1)))) A <- "U" else A <- "S"
+          if (isTRUE(all.equal(xs, c(-1,1)))) A <- "U" else A <- "S"
         }
       }
     }
@@ -42,6 +43,9 @@ function(V, tolerance=0.1) {
   # manually set skew for "J" type distributions (necessary because long tails can change value S):
 	if (A == "J" & max.x < 1)  S <- -1 # single peak at left end  -- "L"
 	if (A == "J" & min.x > -1) S <- 1  # single peak at right end -- "J"
-  r <- list(type= A, skew = S, pattern = x)
+  # "strict" versus "modified"
+  if (variant == "strict" & A == "F") A <- NA              # "F" not defined in AJUS strict
+  if (variant == "modified" & A == "J" & S == -1) A <- "L" # "L" as modified type
+  r <- list(type = A, skew = S, pattern = x)
   return(r)
   }
--- pkg/R/ajusPlot.R
+++ pkg/R/ajusPlot.R
@@ -1,8 +1,8 @@
-ajusPlot <- function(V,tolerance=0.1) {
+ajusPlot <- function(V, tolerance=0.1, variant="modified") {
 # plot AJUS with additional information
 # removing NA to deal with real-life data
 # (e.g. time series with missing data)
-a <- ajus(na.omit(V),tolerance) # do not consider missing values
+a <- ajus(na.omit(V), tolerance=tolerance, variant=variant) # do not consider missing values
 l <- length(V)          # for plotting
 m <- max(V, na.rm=TRUE) # for plotting
 i <- min(V, na.rm=TRUE) # for difference d
@@ -14,6 +14,6 @@ axis(1, at=1:l)	# control axes
 axis(2)
 if (m==0) m <- 1 # avoid plotting type on line if "F" at 0
 if (d==0) d <- m # avoit plotting type on line if "F"
-text(1+l/5,m-d/5,paste("type",a$type, sep=" "),cex=2)
-text(1+l/5,m-d/3,paste("skew",a$skew, sep=" "))
+text(1+l/5, m-d/5, paste("type", a$type, sep=" "), cex=2)
+text(1+l/5, m-d/3, paste("skew", a$skew, sep=" "))
 }
